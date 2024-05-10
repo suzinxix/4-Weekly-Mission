b4adfa010fedf42ef7ddc57be506743ca62dedb6
suzinxix <suzinxix@gmail.com>
2024-05-11 08:29:42 +0900

    chore: tanstack query 설치 및 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,27 @@
-import type { ReactElement, ReactNode } from 'react'
-import type { NextPage } from 'next'
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "styles/globals.css";
 
+const queryClient = new QueryClient();
+
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
- 
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
+  Component: NextPageWithLayout;
+};
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page)
- 
-  return getLayout(<Component {...pageProps} />)
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return getLayout(
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
